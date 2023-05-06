4d65a494c2eea42b854f8002f336e859ef2c0711
Mert YILMAZ <mertyilmaz.dev@gmail.com>
2023-05-06 18:43:12 +0300

    Courses Listing

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -5,12 +5,17 @@ using Photon.Realtime;
 using Photon.Pun;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class NetworkManager : MonoBehaviourPunCallbacks
 {
     [SerializeField] private TMP_InputField joinInputField;
     [SerializeField] private TMP_InputField createInputField;
+    [SerializeField] private GameObject roomElementPrefab;
+    [SerializeField] private Transform roomListContainer;
+
+    private Dictionary<string, RoomInfo> _cachedRoomList = new Dictionary<string, RoomInfo>();
 
     private void Start()
     {
@@ -36,7 +41,7 @@ public class NetworkManager : MonoBehaviourPunCallbacks
             Debug.LogError("Not connected!");
             return;
         }
-        
+
         if (string.IsNullOrEmpty(createInputField.text))
         {
             Debug.LogError("No course name is given!");
@@ -83,5 +88,66 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     {
         base.OnJoinRoomFailed(returnCode, message);
         Debug.LogError("Could not join room!");
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        ClearRooms();
+        foreach (var room in roomList)
+        {
+            if (room.RemovedFromList || !room.IsVisible)
+            {
+                Debug.Log("Removing room, room removed: " + room.RemovedFromList + ", room is visible: " +
+                          room.IsVisible);
+                _cachedRoomList.Remove(room.Name);
+                continue;
+            }
+
+            Debug.Log(room.Name);
+            _cachedRoomList[room.Name] = room;
+        }
+
+        foreach (var room in _cachedRoomList.Values)
+        {
+            GameObject roomElement = Instantiate(roomElementPrefab, roomListContainer) as GameObject;
+            roomElement.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = room.Name;
+            roomElement.GetComponentInChildren<Button>().onClick.AddListener(() => JoinTargetRoom(room.Name));
+        }
+    }
+
+    public override void OnLeftLobby()
+    {
+        ClearRooms();
+    }
+
+    public void JoinTargetRoom(string roomName)
+    {
+        if (PhotonNetwork.InLobby)
+        {
+            LeaveLobby();
+        }
+
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    public void JoinLobby()
+    {
+        if (!PhotonNetwork.InLobby)
+        {
+            PhotonNetwork.JoinLobby();
+        }
+    }
+
+    public void LeaveLobby()
+    {
+        if (PhotonNetwork.InLobby)
+        {
+            PhotonNetwork.LeaveLobby();
+        }
+    }
+
+    private void ClearRooms()
+    {
+        _cachedRoomList = new Dictionary<string, RoomInfo>();
     }
 }
